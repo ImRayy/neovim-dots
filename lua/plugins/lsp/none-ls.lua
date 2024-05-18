50a406ca0ece8becce6dde4bdfe2ee0ff40b2fc1
RAY <souradeep_roy@pm.me>
2024-05-18 21:59:37 +0530

    update none-ls

--- lua/plugins/lsp/none-ls.lua
+++ lua/plugins/lsp/none-ls.lua
@@ -7,18 +7,10 @@ return {
     },
     config = function()
         local null_ls = require("null-ls")
-        local formatting = null_ls.builtins.formatting
-        local diagnostics = null_ls.builtins.diagnostics
-        local completion = null_ls.builtins.completion
-
 
         null_ls.setup({
             debug = false,
             sources = {
-                formatting.prettier,
-                formatting.stylua,
-                formatting.black.with({ extra_args = { "--fast" } }),
-                completion.spell,
                 require("none-ls.diagnostics.eslint"),
                 require("none-ls.diagnostics.flake8"),
             },
