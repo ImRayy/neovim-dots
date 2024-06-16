dd0c8a0def9867fc748b7c97219e7ebec2e9d735
ImRayy <souradeep_roy@pm.me>
2024-06-16 11:39:29 +0530

    fix eslint errors

--- lua/plugins/lsp/linter.lua
+++ lua/plugins/lsp/linter.lua
@@ -1,33 +1,49 @@
 return {
-    'mfussenegger/nvim-lint',
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-        events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-        linters_by_ft = {
-            javascript = { 'eslint_d' },
-            typescript = { 'eslint_d' },
-            javascriptreact = { 'eslint_d' },
-            typescriptreact = { 'eslint_d' },
-            css = { 'stylelint' },
-            scss = { 'stylelint' },
-            fish = { 'fish' },
-            bash = { 'shellcheck' },
-            markdown = { 'markdownlint' },
-        }
-    },
-    config = function(_, opts)
-        local lint = require('lint')
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+		linters_by_ft = {
+			astro = { "eslint_d" },
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			css = { "stylelint" },
+			scss = { "stylelint" },
+			fish = { "fish" },
+			bash = { "shellcheck" },
+			markdown = { "markdownlint" },
+		},
+		linters = {
+			eslint_d = {
+				args = {
+					"--no-warn-ignored",
+					"--format",
+					"json",
+					"--stdin",
+					"--stdin-filename",
+					function()
+						return vim.api.nvim_buf_get_name(0)
+					end,
+				},
+			},
+		},
+	},
 
-        lint.linters_by_ft = opts.linters_by_ft
+	config = function(_, opts)
+		local lint = require("lint")
 
-        local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+		lint.linters_by_ft = opts.linters_by_ft
+		lint.linters = opts.linters
 
-        vim.api.nvim_create_autocmd(opts.events, {
-            group = lint_augroup,
-            callback = function()
-                lint.try_lint()
-            end,
-        })
-    end
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
+		vim.api.nvim_create_autocmd(opts.events, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+	end,
 }
