0e5cead0d0f5c9feb36aab666e54db44387ed38d
ImRayy <souradeep_roy@pm.me>
2024-06-20 11:37:22 +0530

    add lsp_lines

--- init.lua
+++ init.lua
@@ -5,4 +5,10 @@ require("core.cmds")
 require("core.gui")
 
 -- Colorscheme
-vim.cmd.colorscheme("catppuccin")
+vim.cmd.colorscheme("tokyonight-night")
+
+-- Remove or enable if not using lsp_lines.nvim at `lsp/init.lua`; line :32
+vim.diagnostic.config({
+  virtual_text = false,
+  virtual_lines = { only_current_line = false, highlight_whole_line = false },
+})
--- lua/plugins/lsp/linter.lua
+++ lua/plugins/lsp/linter.lua
@@ -1,29 +1,42 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-    linters_by_ft = {
-      bash = { "shellcheck" },
-      markdown = { "markdownlint" },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+      linters_by_ft = {
+        bash = { "shellcheck" },
+        markdown = { "markdownlint" },
+      },
     },
+
+    config = function(_, opts)
+      local lint = require("lint")
+      lint.linters_by_ft = opts.linters_by_ft
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+      vim.api.nvim_create_autocmd(opts.events, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>l", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+    end,
   },
 
-  config = function(_, opts)
-    local lint = require("lint")
-    lint.linters_by_ft = opts.linters_by_ft
-
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-    vim.api.nvim_create_autocmd(opts.events, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
-
-    vim.keymap.set("n", "<leader>l", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-  end,
+  -- Show nvim diagnostics using virtual lines
+  {
+    "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+    event = "LspAttach",
+    -- stylua: ignore
+    keys = {
+      {'<leader>l', function() require("lsp_lines").toggle() end, desc="Toggle LSP lines" }
+    },
+    opts = {},
+  },
 }
