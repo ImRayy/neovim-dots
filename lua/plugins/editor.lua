bf9cb6cbd7b22bd83a48fca12d223dea90c21e12
ImRayy <souradeep_roy@pm.me>
2024-07-23 12:50:38 +0530

    add vim-dadbod & other sql stuff

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -256,4 +256,18 @@ return {
       },
     },
   },
+
+  -- SQL stuff
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    dependencies = {
+      "tpope/vim-dadbod",
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" } },
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
