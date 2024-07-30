02770bc77eeb5ab1b695e2496cfaa5ff2c940f4b
ImRayy <souradeep_roy@pm.me>
2024-07-30 20:31:45 +0530

    add mini.hipatterns

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -33,4 +33,19 @@ return {
     "NvChad/nvim-colorizer.lua",
     opts = {},
   },
+
+  -- Highlight patterns in text. Part of 'mini.nvim' library.
+  {
+    "echasnovski/mini.hipatterns",
+    version = "*",
+    opts = {
+      highlighters = {
+        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+      },
+    },
+  },
 }
