cc683537e194221ee6263183e837782e26c6be0d
ImRayy <souradeep_roy@pm.me>
2024-08-13 02:07:31 +0530

    replace mini.hipatterns with todo-comments.nvim

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -34,18 +34,10 @@ return {
     opts = {},
   },
 
-  -- Highlight patterns in text. Part of 'mini.nvim' library.
+  -- Highlight, list and search todo comments in your projects
   {
-    "echasnovski/mini.hipatterns",
-    version = "*",
-    opts = {
-      highlighters = {
-        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
-      },
-    },
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
   },
 }
