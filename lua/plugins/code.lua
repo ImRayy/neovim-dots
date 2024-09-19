0780bec43877d81c5586e028d0df0024aef1c495
ImRayy <souradeep_roy@pm.me>
2024-09-19 19:52:31 +0530

    update nvim-colorizer

--- lua/plugins/code.lua
+++ lua/plugins/code.lua
@@ -31,7 +31,21 @@ return {
   -- CSS color highlight
   {
     "NvChad/nvim-colorizer.lua",
-    opts = {},
+    opts = {
+      user_default_options = {
+        css = true,
+        virtualtext = "■",
+        names = false,
+      },
+      filetypes = {
+        "*",
+        "!prompt",
+        "!popup",
+        html = { mode = "foreground" },
+        cmp_docs = { always_update = true },
+        css = { names = true },
+      },
+    },
   },
 
   -- Highlight, list and search todo comments in your projects
