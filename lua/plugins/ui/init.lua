bee82595a9d605ca364846c5d59a28aaeec255ea
ImRayy <souradeep_roy@pm.me>
2025-04-25 10:25:34 +0530

    update telescope keybinds

--- lua/plugins/ui/init.lua
+++ lua/plugins/ui/init.lua
@@ -83,7 +83,8 @@ return {
     },
     keys = {
       { "<leader>ff", "<cmd>Telescope find_files<cr>", mode = "n" },
-      { "<leader>bf", "<cmd>Telescope live_grep<cr>", mode = "n" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", mode = "n" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", mode = "n" },
       { "<leader>fh", "<cmd>Telescope help_tags<cr>", mode = "n" },
     },
   },
