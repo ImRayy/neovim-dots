ada239230599b90dfec52a65fb6ee7c1b04f1d0c
ImRayy <souradeep_roy@pm.me>
2025-07-25 02:07:41 +0530

    update colorscheme

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,3 +1,7 @@
+local is_transparent = function()
+  return vim.g.neovide == nil
+end
+
 return {
 
   -- Catppuccin
@@ -7,49 +11,7 @@ return {
     lazy = true,
     opts = {
       flavour = "mocha", -- latte, frappe, macchiato, mocha
-      background = { -- :h background
-        light = "mocha",
-        dark = "mocha",
-      },
-      transparent_background = vim.g.neovide == nil,
-      show_end_of_buffer = false, -- show the '~' characters after the end of buffers
-      term_colors = false,
-      dim_inactive = {
-        enabled = false,
-        shade = "dark",
-        percentage = 0.15,
-      },
-      no_italic = false, -- Force no italic
-      no_bold = false, -- Force no bold
-      styles = {
-        comments = { "italic" },
-        conditionals = { "italic" },
-      },
-      integrations = {
-        alpha = true,
-        cmp = true,
-        gitsigns = true,
-        indent_blankline = { enabled = true },
-        mini = true,
-        neotree = true,
-        noice = true,
-        notify = true,
-        nvimtree = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-        mason = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-      },
+      transparent_background = is_transparent(),
     },
   },
 
@@ -57,6 +19,9 @@ return {
   {
     "folke/tokyonight.nvim",
     lazy = true,
+    opts = {
+      transparent = is_transparent(),
+    },
   },
 
   -- Rose Pine
@@ -67,7 +32,7 @@ return {
     opts = {
       variant = "auto",
       styles = {
-        transparency = vim.g.neovide == nil,
+        transparency = is_transparent(),
       },
     },
   },
@@ -79,7 +44,7 @@ return {
     priority = 1000,
     config = true,
     opts = {
-      transparent_mode = vim.g.neovide == nil,
+      transparent_mode = is_transparent(),
     },
   },
 }
