942f7e3cf04cf201c3cd14636cea7020405e5280
ImRayy <souradeep_roy@pm.me>
2025-07-25 02:13:33 +0530

    remove mini.indentscope

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -32,36 +32,6 @@ return {
     },
   },
 
-  -- Visualize and work with indent scope
-  {
-    "echasnovski/mini.indentscope",
-    event = "LspAttach",
-    version = false,
-    opts = {
-      symbol = "│",
-      options = { try_as_border = true },
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = {
-          "help",
-          "alpha",
-          "neo-tree",
-          "Trouble",
-          "trouble",
-          "lazy",
-          "mason",
-          "notify",
-          "toggleterm",
-          "lazyterm",
-        },
-        callback = function()
-          vim.b.miniindentscope_disable = true
-        end,
-      })
-    end,
-  },
-
   -- Move any selection in any direction
   {
     "echasnovski/mini.move",
